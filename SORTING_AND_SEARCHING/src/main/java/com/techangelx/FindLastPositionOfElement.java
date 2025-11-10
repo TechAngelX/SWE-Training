994b5e3232ebb20ad9f5d9f1dd8438e6603e8940
Ricki Angel <angelricki@outlook.com>
2025-11-10 16:20:52 +0000

    Complete: Binary search implementation for finding LAST  occurrence

--- SORTING_AND_SEARCHING/src/main/java/com/techangelx/FindLastPositionOfElement.java
+++ SORTING_AND_SEARCHING/src/main/java/com/techangelx/FindLastPositionOfElement.java
@@ -1,4 +1,26 @@
 package com.techangelx;
 
+import java.util.List;
+
 public class FindLastPositionOfElement {
+    public static int lastOcc(List<Integer> nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        int lastIndexFound = -1; // default return not found
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums.get(mid) == target) {
+                lastIndexFound = mid;  // store the index
+                left = mid + 1;        // Continue searching right
+                
+            } else if (nums.get(mid) < target) {// target higher than position
+                left = mid + 1; // Search right half
+                
+            } else {
+                right = mid - 1;
+            }   
+        }
+        return lastIndexFound; }
+
 }
