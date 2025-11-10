9f5f0ee8e49ae5d1c0fa83f5bdc4eaf9541083b8
Ricki Angel <angelricki@outlook.com>
2025-11-10 16:20:30 +0000

    Update: Sorting Main Class

--- SORTING_AND_SEARCHING/src/main/java/com/techangelx/Main.java
+++ SORTING_AND_SEARCHING/src/main/java/com/techangelx/Main.java
@@ -1,7 +1,24 @@
 package com.techangelx;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        List<Integer> nums = Arrays.asList(1,1,2,2,3,4,4,5);
+        int target = 4;
+        
+//        FIRST OCCURRENCE
+//        int result = FindFirstPositonOfElement.firstOcc(nums,target);
+//        System.out.println("Target number "+target + " first appears at index position "+ result);
+
+
+//        LAST OCCURRENCE
+//        int result = FindLastPositionOfElement.lastOcc(nums,target);
+//        System.out.println("Target number "+target + " last appears at index position "+ result);
+ 
+ 
+ 
     }
 }
